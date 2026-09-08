54df790a8ac67c53c8cefa85879052c15a347f4a
Muhammad Al-Aqsa <aqsa34354@gmail.com>
2026-09-08 10:51:19 +0700

    Casting(Konversi Tipe Data

--- praktikum-2/src/VariabelDemo.java
+++ praktikum-2/src/VariabelDemo.java
@@ -12,6 +12,14 @@ public class VariabelDemo {
             System.out.println("Golongan Darah: " + golDarah);
             System.out.println("Aktif: " + mahasiswaAktif);
 
+            int nilaiBulat = 9;
+            double nilaiDouble = nilaiBulat; // widening otomatis
+            System.out.println("Widening; " + nilaiDouble);
+
+            double pecahan = 9.8;
+            int hasilCasting = (int) pecahan; // narrowing eksplisit
+            System.out.println("Narrowing: " + hasilCasting);
+
         }
     }
 
