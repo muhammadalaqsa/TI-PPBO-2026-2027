a9a1264a6b32279449c1ffa37afcc72519f88a00
Muhammad Al-Aqsa <aqsa34354@gmail.com>
2026-09-08 11:28:41 +0700

    Input dengan Scanner

---
+++ praktikum-2/src/BiodataSaya.java
@@ -0,0 +1,8 @@
+public class BiodataSaya {
+    public static void main(String[] args) {
+        //Nomor 1
+        String nama = "Muhammad Al-Aqsa";
+        int umur = 19;
+        String NIM = "2025573010079";
+    }
+}
