68c2bdcd647f333b17619df138e7fde865dfb785
Muhammad Al-Aqsa <aqsa34354@gmail.com>
2026-09-08 10:35:48 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/HelloWorld.java
+++ praktikum-2/src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Halo, Aqsa!");
+        System.out.print("Nama: ");
+        System.out.print("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
